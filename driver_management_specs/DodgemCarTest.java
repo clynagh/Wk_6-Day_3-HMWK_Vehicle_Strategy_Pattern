fcdb330dd4606348e356926084819f0980b073fd
Craig Lynagh <craiglynagh@gmail.com>
2017-08-02 20:22:09 +0100

    QuadBikeTest created and driveTime returns successfully

--- driver_management_specs/DodgemCarTest.java
+++ driver_management_specs/DodgemCarTest.java
@@ -5,18 +5,15 @@ import driver_management.*;
 public class DodgemCarTest {
 
   DodgemCar car;
-  QuadBike bike;
 
 
   @Before
   public void before(){
     car = new DodgemCar(10);
-    bike = new QuadBike(10);
   }
 
   @Test
   public void driveDistanceReturnsTime(){
-    assertEquals(1, bike.driveTime(10));
     assertEquals(1, car.driveTime(10));
   }
 }
---
+++ driver_management_specs/QuadBikeTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import driver_management.*;
+
+public class QuadBikeTest {
+
+  QuadBike bike;
+
+  @Before
+  public void before(){
+    bike = new QuadBike(10);
+  }
+
+  @Test
+  public void driveDistanceReturnsTimes(){
+    assertEquals(1, bike.driveTime(10));
+  }
+}
