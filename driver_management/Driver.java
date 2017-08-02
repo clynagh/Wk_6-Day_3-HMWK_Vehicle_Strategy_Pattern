76de106da8637be396bf2d209b043c7a0cb812d7
Craig Lynagh <craiglynagh@gmail.com>
2017-08-02 22:13:27 +0100

    Driver can drive both a quad and dodgem successfully

--- driver_management/Driver.java
+++ driver_management/Driver.java
@@ -3,14 +3,21 @@ import behaviours.*;
 
 public class Driver{
   String name;
+  private Driveable drive;
 
-  public Driver(String name){
+  public Driver(String name, Driveable drive){
   this.name = name;
+  this.drive = drive;
 }
 public String driverName(){
   return this.name;
 }
 
+public void setDrive(Driveable drive){
+    this.drive = drive;
+  }
+
 }
+
 
 
--- driver_management_specs/DriverTest.java
+++ driver_management_specs/DriverTest.java
@@ -5,14 +5,31 @@ import driver_management.*;
 public class DriverTest {
 
   Driver driver;
+  QuadBike bike;
+  DodgemCar car;
 
   @Before
   public void before(){
-    driver = new Driver("Pat");
+    driver = new Driver("Pat", bike);
+    bike = new QuadBike(10, 50);
+    car = new DodgemCar(10, 2);
   }
 
   @Test
   public void getDriverName(){
     assertEquals("Pat", driver.driverName());
+  }
+  @Test
+  public void canSetDriveQuad(){
+    QuadBike bike = new QuadBike(10, 50);
+    driver.setDrive(bike);
+    assertEquals("Vrooom, off we go!!", bike.drive());
+  }
+
+  @Test
+  public void canSetDriveDodgem(){
+    DodgemCar car = new DodgemCar(10, 2);
+    driver.setDrive(car);
+    assertEquals("Forward, BUMP, backwards, BUMP!!", car.drive());
   }
 }
